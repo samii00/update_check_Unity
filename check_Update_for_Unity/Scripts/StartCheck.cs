91e1f48e79221921ae33fca76328720ff7938051
samii00 <40570047+samii00@users.noreply.github.com>
2024-02-07 14:35:45 +0100

    Add files via upload

---
+++ check_Update_for_Unity/Scripts/StartCheck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+
+//attach this script to a gameObject
+public class StartCheck : MonoBehaviour
+{
+	public Start()
+        var updatCheker = UpdateAppCheck.Instacne;
+		updatCheker.updateCheck();
+	}
+}
